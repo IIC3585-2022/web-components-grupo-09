4c039cd49a4d6ac7e5bad15286c48ebe62799e2c
julio <jnandrade@uc.cl>
2022-06-14 16:25:52 -0400

    Resolve web-components errors

--- src/shop/item.js
+++ src/shop/item.js
@@ -1,4 +1,4 @@
-export const templateShopItem = document.createElement('template');
+const templateShopItem = document.createElement("template");
 templateShopItem.innerHTML = `
 <style>
 .info {
@@ -58,81 +58,79 @@ img {
 `;
 
 class ShopItem extends HTMLElement {
-    constructor() {
-        super();
-        // when this is open, need to use 
-        // shadowRoot for disasembling
-        this.attachShadow({ 'mode': 'open' });
-        this.descriptionValue = false;
-        this.priceValue = false;
-        this.discountValue = false;
-        this.imgSourceValue = false;
-    };
+  constructor() {
+    super();
+    // when this is open, need to use
+    // shadowRoot for disasembling
+    this.attachShadow({ mode: "open" });
+    this.descriptionValue = false;
+    this.priceValue = false;
+    this.discountValue = false;
+    this.imgSourceValue = false;
+  }
 
-    connectedCallback() {
-        // It's called when is mounted on html
-        this.shadowRoot.appendChild(
-            templateShopItem.content.cloneNode(true)
-        );
-        // Add shadow root for style only on the component
-        this.imgElement = this.shadowRoot.querySelector('img');
-        this.infoElement = this.shadowRoot.querySelector('.info');
-        this.descriptionElement = this.shadowRoot.querySelector('.description');
-        this.priceElement = this.shadowRoot.querySelector('#price');
-        this.discountElement = this.shadowRoot.querySelector('.discount');
-        this.normalPriceElement = this.shadowRoot.querySelector('.price');
-        this._render();
-    };
+  connectedCallback() {
+    // It's called when is mounted on html
+    this.shadowRoot.appendChild(templateShopItem.content.cloneNode(true));
+    // Add shadow root for style only on the component
+    this.imgElement = this.shadowRoot.querySelector("img");
+    this.infoElement = this.shadowRoot.querySelector(".info");
+    this.descriptionElement = this.shadowRoot.querySelector(".description");
+    this.priceElement = this.shadowRoot.querySelector("#price");
+    this.discountElement = this.shadowRoot.querySelector(".discount");
+    this.normalPriceElement = this.shadowRoot.querySelector(".price");
+    this._render();
+  }
 
-    static get observedAttributes() {
-        return ['description'];
-    }
-    attributeChangedCallback(name, oldValue, newValue) {
-        this.descriptionValue = newValue;
-    }
+  static get observedAttributes() {
+    return ["description"];
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    this.descriptionValue = newValue;
+  }
 
-    set index(value) {
-        this.indexValue = value;
-    };
+  set index(value) {
+    this.indexValue = value;
+  }
 
-    get index() {
-        return this.indexValue;
-    };
+  get index() {
+    return this.indexValue;
+  }
 
-    set price(value) {
-        this.priceValue = parseInt(value);
-    };
+  set price(value) {
+    this.priceValue = parseInt(value);
+  }
 
-    get price() {
-        return this.priceValue;
-    };
+  get price() {
+    return this.priceValue;
+  }
 
-    set discount(value) {
-        this.discountValue = parseInt(value);
-    };
+  set discount(value) {
+    this.discountValue = parseInt(value);
+  }
 
-    get discount() {
-        return this.discountValue;
-    };
+  get discount() {
+    return this.discountValue;
+  }
 
-    set imgSource(value) {
-        this.imgSourceValue = value;
-    };
+  set imgSource(value) {
+    this.imgSourceValue = value;
+  }
 
-    get imgSource() {
-        return this.imgSourceValue;
-    };
+  get imgSource() {
+    return this.imgSourceValue;
+  }
 
-    _render() {
-        if (!this.infoElement) return;
-        this.descriptionElement.textContent = this.descriptionValue;
-        this.priceElement.textContent = `$${Math.round(
-            this.priceValue * (1 - this.discountValue / 100)
-        )}`;
-        this.discountElement.textContent = `${this.discountValue}%`;
-        this.normalPriceElement.textContent = `$${this.priceValue}`;
-        this.imgElement.setAttribute('src', this.imgSourceValue)
-    };
-};
+  _render() {
+    if (!this.infoElement) return;
+    this.descriptionElement.textContent = this.descriptionValue;
+    this.priceElement.textContent = `$${Math.round(
+      this.priceValue * (1 - this.discountValue / 100)
+    )}`;
+    this.discountElement.textContent = `${this.discountValue}%`;
+    this.normalPriceElement.textContent = `$${this.priceValue}`;
+    this.imgElement.setAttribute("src", this.imgSourceValue);
+  }
+}
 
-window.customElements.define('shop-item', ShopItem);
+window.customElements.define("shop-item", ShopItem);
